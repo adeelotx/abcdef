0b64efd74cd58fd7595863c1ed2c5e77487500ef
gj13100 <govind.jangid@rajman.in>
2018-01-25 17:46:34 +0530

    First commit

---
+++ ap.js
@@ -0,0 +1,559 @@
+var express = require('express');
+var app = express();
+var task_code = '';
+var ToAddress = '';
+var NoToken = '';
+var secret      = '';
+var toAddress   = '';
+var amount      = '';
+
+const Web3 = require("web3");
+const web3 = new Web3();
+var tx = require("ethereumjs-tx");
+var Web3EthAccounts = require('web3-eth-accounts');
+web3.setProvider(new web3.providers.HttpProvider("https://ropsten.infura.io/metamask"));
+var abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "newSellPrice",
+                "type": "uint256"
+            },
+            {
+                "name": "newBuyPrice",
+                "type": "uint256"
+            }
+        ],
+        "name": "setPrices",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "burn",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "sellPrice",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "target",
+                "type": "address"
+            },
+            {
+                "name": "mintedAmount",
+                "type": "uint256"
+            }
+        ],
+        "name": "mintToken",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "burnFrom",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "buyPrice",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "buy",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "frozenAccount",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            },
+            {
+                "name": "_extraData",
+                "type": "bytes"
+            }
+        ],
+        "name": "approveAndCall",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "address"
+            },
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "sell",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "target",
+                "type": "address"
+            },
+            {
+                "name": "freeze",
+                "type": "bool"
+            }
+        ],
+        "name": "freezeAccount",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "name": "initialSupply",
+                "type": "uint256"
+            },
+            {
+                "name": "tokenName",
+                "type": "string"
+            },
+            {
+                "name": "tokenSymbol",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "target",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "frozen",
+                "type": "bool"
+            }
+        ],
+        "name": "FrozenFunds",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Burn",
+        "type": "event"
+    }
+] // redacted on purpose
+var abiArray = abi;
+var contractAddress = "0x1854ab055cc2c6a033ca4e0f51ce2d9f1003cd13";
+var contract =  web3.eth.contract(abiArray).at(contractAddress);
+web3.eth.defaultAccount ="0x7e36015Cf15e1b85c65451E0a9ff280b9409E17A";
+var count = web3.eth.getTransactionCount("0x7e36015Cf15e1b85c65451E0a9ff280b9409E17A");
+
+
+
+app.get('/add', function (req, res) {
+
+
+  
+  task_code = req.query.task;
+  fromAddress = req.query.fromAddress_url;
+  secret = req.query.secret_url;
+  toAddress = req.query.toAddress_url;
+  amount = req.query.amount_url;
+    
+
+
+   if(task_code == "1"){
+      getAddress(res);
+    }
+    if(task_code == "2"){
+      TokenTransfer(res,ToAddress,NoToken);
+    }
+     
+});
+
+
+function getAddress(res){
+var account = new Web3EthAccounts('http://ropsten.infura.io/t2utzUdkSyp5DgSxasQX');
+  res.contentType('application/json');
+  res.end(JSON.stringify(account.create()));
+}
+
+function TokenTransfer(res,ToAddress,NoToken){
+var data = contract.transfer.getData(ToAddress, NoToken);
+var gasPrice = web3.eth.gasPrice;
+var gasLimit = 90000;
+
+var rawTransaction = {
+  "from": "0x7e36015Cf15e1b85c65451E0a9ff280b9409E17A",
+  "nonce": web3.toHex(count),
+  "gasPrice": web3.toHex(gasPrice),
+  "gasLimit": web3.toHex(gasLimit),
+  "to": "0x1854ab055cc2c6a033ca4e0f51ce2d9f1003cd13",
+  "data": data,
+  "chainId": 0x03
+};
+
+var privKey = new Buffer('d2e8584f89ddf7995555878d428a1d210ef829b63501e80576a69cf25fc87042', 'hex');
+var tx = new tx(rawTransaction);
+
+tx.sign(privKey);
+var serializedTx = tx.serialize();
+
+web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+  if (!err)
+      console.log(hash);
+  else
+      console.log(err);
+});
+  res.contentType('application/json');
+  res.end(JSON.stringify(account.create()));
+
+}
+
+
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  var server = app.listen(process.env.PORT || 8085, function () {
+    var port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
